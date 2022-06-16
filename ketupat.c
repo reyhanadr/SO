39f3c8164e9264a8c0c2fa3b5fb3c753e9523083
reyhanadr <88282489+reyhanadr@users.noreply.github.com>
2022-06-16 07:48:52 +0700

    Create ketupat.c

---
+++ ketupat.c
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+  cout << "##  Program C++ Belah Ketupat Bintang  ##" << endl;
+  cout << "=========================================" << endl;
+  cout << endl;
+ 
+  int lebar_belah_ketupat,i,j,k;
+ 
+  cout << "Input lebar belah ketupat: ";
+  cin >> lebar_belah_ketupat;
+ 
+  cout << endl;
+ 
+  for(i=1;i<=lebar_belah_ketupat;i++) {
+ 
+    for(j=1;j<=lebar_belah_ketupat-i;j++) {
+      cout << " ";
+    }
+ 
+    for(k=1;k<=i;k++) {
+      cout << " *";
+    }
+    cout << endl;
+  }
+ 
+  for(i=1;i<lebar_belah_ketupat;i++) {
+ 
+    for(j=1;j<=i;j++) {
+      cout << " ";
+    }
+ 
+    for(k=1;k<=lebar_belah_ketupat-i;k++) {
+      cout << " *";
+    }
+    cout << endl;
+  }
+ 
+  return 0;
+}
